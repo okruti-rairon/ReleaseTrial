171d94c21deedeaab1a2b600266f7f530e6ba3a6
Rudraksh Airon <rairon@okruti.com>
2025-09-11 17:22:26 +0530

    project_changes

---
+++ src/main/java/com/example/trials/TrialsApplication.java
@@ -0,0 +1,13 @@
+package com.example.trials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrialsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/trials/TrialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
